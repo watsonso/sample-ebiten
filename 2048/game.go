fd4edf9022d2df5a7d546821e65934f402a7dd15
watsonso <uni0360@gmail.com>
2021-07-25 22:51:31 +0900

    Create board

---
+++ 2048/game.go
@@ -0,0 +1,38 @@
+package twenty48
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+const (
+	ScreenWidth = 420
+	ScreenHeight = 600
+	boardSize = 4
+)
+
+// Game represents a game state.
+type Game struct {
+	input *Input
+	board *Board
+	boardImage *ebiten.Image
+}
+
+// NewGame generates a new Game object.
+func NewGame() (*Game, error) {
+	g := &Game{
+		input: NewInput(),
+	}
+	var err error
+	g.board, err = NewBoard(boardSize)
+	if err != nil {
+		return nil, err
+	}
+	return g, nil
+}
